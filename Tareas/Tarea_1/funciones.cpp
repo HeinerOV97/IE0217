c165797d0de5401170849eba097230b33937a51f
HeinerOV97 <heiner.obando@ucr.ac.cr>
2024-01-08 23:20:27 -0600

    Se agrega documentacion a los archivos
    Signed-off-by: HeinerOV97 <heiner.obando@ucr.ac.cr>

--- Tareas/Tarea_1/funciones.cpp
+++ Tareas/Tarea_1/funciones.cpp
@@ -3,6 +3,9 @@
 #include <random>
 #include <time.h>
 
+/**
+ * @brief mostraMenu Muestra el menu de opciones al usuario no recibe parametros
+*/
 void mostrarMenu(){
     std::cout << "\n --- Menu ---\n";
     std::cout << "1. Intervalo para adivinar el número\n";
@@ -11,10 +14,16 @@ void mostrarMenu(){
     std::cout << "4. Salir\n";
 }
 
+/**
+ * @brief elegirOpcion Recibe la opcion escogida por el usuario en el menu y a partir de eso llama a la funcion indicada
+ * @param ptr apunta a la direccion de almacenamiento de la variable datos creada a partir de la estructura datosJuego
+ * @param datos se pasa la referencia a la variable datos creada a partir de la estructura datosJuego
+*/
 void elegirOpcion(datosJuego *ptr, datosJuego &datos){
-    int opcion;
+
+    int opcion; // Se declara la variable local opcion, donde se almacenara la opcion elegida por el usuario en el menu
     std::cout << "Ingrese una opcion: ";
-    std::cin >> opcion;
+    std::cin >> opcion; //Se recibe la opcion escogida por el usuario
 
     switch (opcion){
         case 1: //Definir intervalo del juego
@@ -29,24 +38,32 @@ void elegirOpcion(datosJuego *ptr, datosJuego &datos){
         case 4: //Salir
             std::cout << "Saliendo del programa...\n";
             exit(0);
-        default:
+        default: //Opcion incorrecta
             std::cout << "Opcion no valida. Intente de nuevo...\n";
     }
 }
 
+/**
+ * @brief definirIntervalo funcion encargada de crear un numero aleatorio a partir de un intervalo escogido por el usuario
+ * @param ptr apunta a la direccion de almacenamiento de la variable datos creada a partir de la estructura datosJuego
+ * @param datos se pasa la referencia a la variable datos creada a partir de la estructura datosJuego
+*/
 void definirIntervalo(datosJuego *ptr, datosJuego &datos){
     
-    int valorParaAdvinar, valorRandom;
+    int valorRandom; //Se declara la variable al usurio donde se guardara el valor aleatorio
     
-    
+    //Se solicita al usuario el primero numero del intervalo
     std::cout << "Defina el primer valor del intervalo" << std::endl;
     std::cin >> ptr->primerValor;
 
+    //Se solicita al usuario el segundo numero del intervalo
     std::cout << "Defina el primer valor del intervalo" << std::endl;
     std::cin >> ptr->segValor;
 
     std::cout << datos.primerValor << " " << datos.segValor << std::endl; //Borrar es para pruebas
 
+    /*Si el valor ingresado por el usuario de primero es mayor que el segundo se invierten, esto para evitar conflictos
+    al obtener el numero aleatorio*/
     if(datos.primerValor > datos.segValor){
         int cambioPos = datos.primerValor;
         datos.primerValor = datos.segValor;
@@ -54,15 +71,23 @@ void definirIntervalo(datosJuego *ptr, datosJuego &datos){
         std::cout << datos.primerValor << datos.segValor << std::endl; //Borrar es para pruebas
     }
 
-    srand(time(0));
-    valorRandom = (rand() % (datos.segValor - datos.primerValor + 1)) + datos.primerValor;
-    datos.numRandom = valorRandom;
-    datos.numIntentos = (datos.segValor - datos.primerValor) / 3;
+    //Se utiliza el metodo rand() para obtener el numero aleatorio
+    srand(time(0)); //srand crea la semilla para obtener el numero aleatorio
+    //Sintaxis para obtener un numero aleatorio entre un intervalo
+    valorRandom = (rand() % (datos.segValor - datos.primerValor + 1)) + datos.primerValor; 
+    datos.numRandom = valorRandom; //Se almacena el numero aleatorio en un dato de la variable estructura
+    datos.numIntentos = (datos.segValor - datos.primerValor) / 3; //Se calcula el numero de intentos que tendra el usuario
 
 }
 
+/**
+ * @brief definirDificultad funcion encargada de definir la dificultad del juego a partir de la desicion del usuario
+ * @param ptr apunta a la direccion de almacenamiento de la variable datos creada a partir de la estructura datosJuego
+ * @param datos se pasa la referencia a la variable datos creada a partir de la estructura datosJuego
+*/
 void definirDificultad(datosJuego *ptr, datosJuego &datos){
-    int opcion;
+    int opcion; // Se declara la variable local opcion, donde se almacenara la opcion elegida por el usuario para la dificultad
+    //Se explican al usuario los dos modos de juego
     std::cout << "Defina la dificultad con la que desea jugar:\n" << std::endl;
     std::cout << "1. Facil: Tendrá un tercio del intervalo escogido de oportunidades para adivinar el número."
                 << " El juego le indicará si el numero que prueba es mayor o menos al numero objetivo." << std::endl;
@@ -70,8 +95,9 @@ void definirDificultad(datosJuego *ptr, datosJuego &datos){
                 << " El juego le indicará si el numero que prueba es cercano o no al numero objetivo por medio de palabras clave."
                 << " Estas serán: Congelado, Frio, Caliente o hirviendo" << std::endl;
     std::cout << "Ingrese una opcion: ";
-    std::cin >> opcion;
-                
+    std::cin >> opcion; // Se recibe la opcion escogida por el usuario
+    
+    // Se define la dificultad a partir de la opcion dada por el usuario
     switch (opcion){
         case 1: //Definir intervalo del juego
             datos.dificultad = 1;
@@ -80,15 +106,21 @@ void definirDificultad(datosJuego *ptr, datosJuego &datos){
             datos.dificultad = 2;
             break;
         default:
-            std::cout << "Opcion no valida. Intente de nuevo...\n\n";
+            std::cout << "Opcion no valida. Intente de nuevo...\n\n"; //Si no define un valor adecuado se le solicita de nuevo
             definirDificultad(ptr, datos);
     }
 }
 
+/**
+ * @brief iniciaJuego Esta funcion lleva a cabo el juego, tiene los comandos necesarios para que el juego se lleve acabo en las 2 dificultades
+ * @param ptr apunta a la direccion de almacenamiento de la variable datos creada a partir de la estructura datosJuego
+ * @param datos se pasa la referencia a la variable datos creada a partir de la estructura datosJuego
+*/
 void iniciaJuego(datosJuego *ptr, datosJuego &datos){
-    int numeroPrueba;
-    int opcion;
+    int numeroPrueba; //Variable donde se almacena el numero que intenta el usuario
+    int opcion; // Se declara la variable local opcion, donde se almacenara la opcion de reintentar o volver al menu principal
 
+    //Se verifica si el usuario escogio un intervalo valido
     if(datos.primerValor == 0 && datos.segValor == 0){
         std::cout << "No a seleccionado un rango correcto para iniciar el juego" << std::endl;
         return;
@@ -96,92 +128,96 @@ void iniciaJuego(datosJuego *ptr, datosJuego &datos){
         std::cout << "Los valores ingresados para el intervalo son iguales, seleccione un rango correcto para iniciar el juego" << std::endl;
         return;
     } else {
-        std::cout << "\n---Hora de iniciar el juego---\n" << std::endl;
-        if (datos.dificultad == 1){
+        std::cout << "\n---Hora de iniciar el juego---\n" << std::endl; //Se inicia el juego
+        if (datos.dificultad == 1){ //Modo facil
             std::cout << "Numero para adivinar: " << datos.numRandom << "\n"; //borrar
             std::cout << "Numero de intentos: " << datos.numIntentos << std::endl; //borrar
-            for(int i = 0; i < datos.numIntentos; ++i){
+            for(int i = 0; i < datos.numIntentos; ++i){ //Se hace un for que se repita el numero de intentos que posee el usuario
                 std::cout << "Ingresa un numero he intenta llegar al numero objetivo" << std::endl;
-                std::cin >> numeroPrueba;
-                if (numeroPrueba > datos.segValor || numeroPrueba < datos.primerValor){
+                std::cin >> numeroPrueba; //Se recibe el valor indicado por el usuario para intentar adivinar
+                if (numeroPrueba > datos.segValor || numeroPrueba < datos.primerValor){ //Se verifica si el valor esta dentro del rango
                     std::cout << "Escogio un valor fuera del rango, ha perdido un turno" << std::endl;
-                    continue;
+                    continue; //Si el valor esta fuera del rango pierde un turno
                 } else {
-                    if (numeroPrueba > datos.numRandom){
-                        std::cout << "Escogio un valor mayor que el numero objetivo, ha perdido un turno, le quedan " 
-                                << datos.numIntentos - i << std::endl;
-                        continue;
-                    } else if (numeroPrueba < datos.numRandom) {
-                        std::cout << "Escogio un valor menor que el numero objetivo, ha perdido un turno, le quedan "
-                                << datos.numIntentos - i << std::endl;
-                        continue;
-                    } else if (numeroPrueba == datos.numRandom){
+                    if (numeroPrueba > datos.numRandom){ //Se verifica si el valor es mayor al numero buscado
+                        std::cout << "Escogio un valor mayor que el numero objetivo, ha perdido un turno, le quedan " << std::endl;
+                        continue; //Si el valor es mayor pierde un turno
+                    } else if (numeroPrueba < datos.numRandom) { //Se verifica si el valor es menor al numero buscado
+                        std::cout << "Escogio un valor menor que el numero objetivo, ha perdido un turno, le quedan " << std::endl;
+                        continue; //Si el valor es mayor pierde un turno
+                    } else if (numeroPrueba == datos.numRandom){ //Se verifica si el valor es igual al numero buscado
                         std::cout << "Felicidades a escogido el numero objetivo" << std::endl;
                         std::cout << "Volviendo al menu principal..." << std::endl;
-                        return;
+                        return; //Si gana vuelve al menu principal
                     }
                 }
             }
+            //Si pierda se le avisa que ha perdido, y se le da oportunidad de reintentar o volver al menu principal
             std::cout << "Ha perdido su oportunidad para adivinar el numero" << std::endl;
             std::cout << "1. Para reintertarlo" << std::endl;
             std::cout << "2. Volver al menu principal" << std::endl;
-            std::cin >> opcion;
+            std::cin >> opcion; //Se recibe decision del usuario
                 
             switch (opcion){
-            case 1: //Definir intervalo del juego
+            case 1: //Reintentar adivinar
                 iniciaJuego(ptr, datos);
                 break;
-            case 2: //Definir la dificultad del juego
+            case 2: //Volver al menu principal
                 break;
             default:
-                std::cout << "Opcion no valida. Intente de nuevo...\n\n";
+                std::cout << "Opcion no valida. Volviendo al menu principal...\n\n";
             }
         // Modo dificil
         } else if (datos.dificultad == 2) {
-            std::cout << "Numero para adivinar: " << datos.numRandom << "\n";
-            std::cout << "Numero de intentos: " << datos.numIntentos << std::endl;
-            for(int i = 0; i < datos.numIntentos; ++i){
+            std::cout << "Numero para adivinar: " << datos.numRandom << "\n"; //Borrar
+            std::cout << "Numero de intentos: " << datos.numIntentos << std::endl; //Borrar
+            for(int i = 0; i < datos.numIntentos; ++i){  //Se hace un for que se repita el numero de intentos que posee el usuario
                 std::cout << "Ingresa un numero he intenta llegar al numero objetivo" << std::endl;
-                std::cin >> numeroPrueba;
-                if (numeroPrueba > datos.segValor || numeroPrueba < datos.primerValor){
+                std::cin >> numeroPrueba; //Se recibe el valor indicado por el usuario para intentar adivinar
+                if (numeroPrueba > datos.segValor || numeroPrueba < datos.primerValor){ //Se verifica si el valor esta dentro del rango
                     std::cout << "Escogio un valor fuera del rango, ha perdido un turno" << std::endl;
-                    continue;
+                    continue; //Si el valor esta fuera del rango pierde un turno
                 } else {
+                    //Se verifica si el numero escogido es mayor o menor por 1 al numero objetivo, esto indica que esta hirviendo
                     if (numeroPrueba == datos.numRandom + 1 || numeroPrueba == datos.numRandom - 1){
                         std::cout << "¡Esta hirviendo! " << std::endl;
-                        continue;
+                        continue; //Pierde un turno
+                    /*Se verifica si el numero escogido es mayor o menor por una diferencia entre 2 y 4 al numero objetivo,
+                    esto indica que esta caliente*/
                     } else if ((numeroPrueba <= datos.numRandom - 2 && numeroPrueba >= datos.numRandom - 4) || 
                                 (numeroPrueba >= datos.numRandom + 2 && numeroPrueba <= datos.numRandom + 4)) {
                         std::cout << "¡Esta caliente!" << std::endl;
-                        continue;
-
+                        continue; //Pierde un turno
+                    /*Se verifica si el numero escogido es mayor o menor por una diferencia entre 5 y 8 al numero objetivo,
+                    esto indica que esta frio*/
                     } else if ((numeroPrueba < datos.numRandom - 4 && numeroPrueba >= datos.numRandom - 8) || 
-                                (numeroPrueba > datos.numRandom + 2 && numeroPrueba <= datos.numRandom + 8)) {
-                        std::cout << "¡Esta frio!" << std::endl;
-
+                                (numeroPrueba > datos.numRandom + 4 && numeroPrueba <= datos.numRandom + 8)) {
+                        std::cout << "¡Esta frio!" << std::endl; //Pierde un turno
+                    /*Se verifica si el numero escogido es mayor o menor por una diferencia de 8 o mas al numero objetivo,
+                    esto indica que esta congelado*/
                     } else if (numeroPrueba < datos.numRandom - 8 || numeroPrueba > datos.numRandom + 8) {
-                        std::cout << "¡Esta congelado!" << std::endl;
-
-                    } else if (numeroPrueba == datos.numRandom){
+                        std::cout << "¡Esta congelado!" << std::endl; //Pierde un turno
+                    } else if (numeroPrueba == datos.numRandom){ // Se verifica si el valor es igual al numero buscado
                         std::cout << "Felicidades a escogido el numero objetivo" << std::endl;
                         std::cout << "Volviendo al menu principal..." << std::endl;
-                        return;
+                        return; // Si gana vuelve al menu principal
                     }
                 }
             }
+            //Si pierda se le avisa que ha perdido, y se le da oportunidad de reintentar o volver al menu principal
             std::cout << "Ha perdido su oportunidad para adivinar el numero" << std::endl;
             std::cout << "1. Para reintertarlo" << std::endl;
             std::cout << "2. Volver al menu principal" << std::endl;
-            std::cin >> opcion;
-                
+            std::cin >> opcion; //Se recibe decision del usuario
+            
             switch (opcion){
-            case 1: //Definir intervalo del juego
+            case 1: //Reintentar adivinar
                 iniciaJuego(ptr, datos);
                 break;
-            case 2: //Definir la dificultad del juego
+            case 2: //Volver al menu del juego
                 break;
             default:
-                std::cout << "Opcion no valida. Intente de nuevo...\n\n";
+                std::cout << "Opcion no valida. Volviendo al menu principal...\n\n";
             }
             
         }
--- Tareas/Tarea_1/main.cpp
+++ Tareas/Tarea_1/main.cpp
@@ -3,12 +3,12 @@
 
 int main() {
 
-    datosJuego *ptr, datos;
-    ptr = &datos;
+    datosJuego *ptr, datos; //Se crea la variable puntero de tipo datosJuegos y una variable tipo datosJuego
+    ptr = &datos; //Se asigna al puntero la direccion en memorio de la variable datos
 
     while(1){
-        mostrarMenu();
-        elegirOpcion(ptr, datos);
+        mostrarMenu(); //Llama a la funcion encargada de mostrar el menu al usuario
+        elegirOpcion(ptr, datos); //Llama a la funcion que permite al usuario elegir opciones en el menu
     }
     return 0;
 
