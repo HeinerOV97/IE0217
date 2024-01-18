c845c78886b93c7416bc167c945b7649547dab68
HeinerOV97 <heiner.obando@ucr.ac.cr>
2024-01-18 17:34:22 -0600

    Se agrega el laboratorio 3, con sus codigos y makefile correspondiente
    Signed-off-by: HeinerOV97 <heiner.obando@ucr.ac.cr>

---
+++ Laboratorios/Laboratorio_3/main.cpp
@@ -0,0 +1,39 @@
+#include "Agenda.hpp"
+#include <iostream>
+
+int main(){
+
+    // Objetos de la clase contacto
+    Contacto<std::string> contacto1("Juanito Mora", "27681234", "dj.juanito@gmail.com");
+    Contacto<std::string> contacto2("William Walker", "865488997", "willywalk@outlook.es");
+    Contacto<std::string> contacto3("Juan Santamaria", "22315689", "adiosmezon@gmail.com");
+
+    // Crear un objeto agenda
+    Agenda<std::string> agenda;
+
+    //Agregar contactos a la agenda
+    try {
+        agenda.agregarContacto(contacto1);
+        agenda.agregarContacto(contacto2);
+        agenda.agregarContacto(contacto3);
+
+    } catch (const std::exception& e){
+        std::cerr << "Error al agregar contacto: " << e.what() << std::endl;
+    }
+
+    //Mostrar contactos
+    std::cout << "Contactos en la agenda: " << std::endl;
+    agenda.mostrarContactos();
+
+    //Eliminar un contacto
+    try {
+        agenda.eliminarContacto("865488997");
+    } catch (const std::exception& e) {
+        std::cerr << "Error al eliminar contacto: " << e.what() << std::endl;
+    }
+
+    //Mostrar contactos
+    std::cout << "Contactos en la agenda: " << std::endl;
+    agenda.mostrarContactos();
+
+}
