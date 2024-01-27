7f4c9a3f3dc981560a8819f523069509b8c70b66
HeinerOV97 <heiner.obando@ucr.ac.cr>
2024-01-27 15:53:52 -0600

    Se agrega la clase alergia que incorpora una lista por defecto de alergia
    Signed-off-by: HeinerOV97 <heiner.obando@ucr.ac.cr>

---
+++ Tareas/TAREA_CUATRO/src/Alergia.py
@@ -0,0 +1,50 @@
+'''
+Se crea la clase Alergia que contiene
+las alergias que estaran por defecto en
+el sistema para probar
+'''
+
+
+class Alergia:
+    # En el inicializador se crea un diccionario
+    # que almacene los datos por defecto del
+    # del sistema
+    def __init__(self):
+        self.alergias = {
+            "huevos": 1,
+            "cacahuetes": 2,
+            "mariscos": 4,
+            "fresas": 8,
+            "tomates": 16,
+            "chocolate": 32,
+            "polen": 64,
+            "gatos": 128,
+            "sardina": 256,
+            "gluten": 512,
+            "huevo": 1024
+        }
+
+    '''
+    Se crea un metodo que va a iterar por el diccionario
+    y va a mostrar en pantalla los elementos que se
+    encuentran en el diccionario, es decir la alergia
+    y su puntuación
+    '''
+    def imprimir_todas_alergias(self):
+        print("Lista de alergias:")
+        # Iteracion sobre cada elemento del diccionario
+        for alergia, valor in self.alergias.items():
+            print(f"{alergia}: {valor}")
+
+    '''
+    Se crea un metodo que va a recibir el nombre de la alergia
+    y va a imprimir su informacion
+    '''
+    def imprimir_alergia_especifica(self, nombre_alergia):
+        # Se comprueba si la alergia existe en la lista
+        # si existe se imprime
+        if nombre_alergia in self.alergias:
+            print(f"{nombre_alergia}: {self.alergias[nombre_alergia]}")
+        # Si no existe se indica al usuario
+        else:
+            print("La alergia no existe en nuestra lista.")
