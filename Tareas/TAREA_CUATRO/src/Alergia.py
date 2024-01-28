fef543344970bbbe2be9fdd4cb7f1eef8fed2e0d
HeinerOV97 <heiner.obando@ucr.ac.cr>
2024-01-28 12:24:27 -0600

    Se modifica el uso de comentarios para un mejor entendimiento
    Signed-off-by: HeinerOV97 <heiner.obando@ucr.ac.cr>

--- Tareas/TAREA_CUATRO/src/Alergia.py
+++ Tareas/TAREA_CUATRO/src/Alergia.py
@@ -1,11 +1,9 @@
-'''
-Se crea la clase Alergia que contiene
-las alergias que estaran por defecto en
-el sistema para probar
-'''
-
-
 class Alergia:
+    '''
+    La clase Alergia contiene
+    las alergias que estaran por defecto en
+    el sistema para probar
+    '''
     # En el inicializador se crea un diccionario
     # que almacene los datos por defecto del
     # del sistema
@@ -24,23 +22,23 @@ class Alergia:
             "huevo": 1024
         }
 
-    '''
-    Se crea un metodo que va a iterar por el diccionario
-    y va a mostrar en pantalla los elementos que se
-    encuentran en el diccionario, es decir la alergia
-    y su puntuación
-    '''
     def imprimir_todas_alergias(self):
+        '''
+        Se crea un metodo que va a iterar por el diccionario
+        y va a mostrar en pantalla los elementos que se
+        encuentran en el diccionario, es decir la alergia
+        y su puntuación
+        '''
         print("Lista de alergias:")
         # Iteracion sobre cada elemento del diccionario
         for alergia, valor in self.alergias.items():
             print(f"{alergia}: {valor}")
 
-    '''
-    Se crea un metodo que va a recibir el nombre de la alergia
-    y va a imprimir su informacion
-    '''
     def imprimir_alergia_especifica(self, nombre_alergia):
+        '''
+        Se crea un metodo que va a recibir el nombre de la alergia
+        y va a imprimir su informacion
+        '''
         # Se comprueba si la alergia existe en la lista
         # si existe se imprime
         if nombre_alergia in self.alergias:
@@ -50,9 +48,9 @@ class Alergia:
         else:
             print("La alergia no existe en nuestra lista.")
 
-    '''
-    Se crea un metodo que permita al usuario ingresar una nueva alergia
-    en la lista predeterminadad
-    '''
     def agregar_alergia(self, nombre, puntaje):
+        '''
+        Se crea un metodo que permita al usuario ingresar una nueva alergia
+        en la lista predeterminadad
+        '''
         self.alergias[nombre] = puntaje
