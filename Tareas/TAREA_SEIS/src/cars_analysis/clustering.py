4c18e6f9478f03cc0000b3813f8b5b96d02d5999
HeinerOV97 <heiner.obando@ucr.ac.cr>
2024-02-12 19:00:04 -0600

    Se agrega dos funciones mas para realizar un segundo clustering con el codigo
    Signed-off-by: HeinerOV97 <heiner.obando@ucr.ac.cr>

--- Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
+++ Tareas/TAREA_SEIS/src/cars_analysis/clustering.py
@@ -9,10 +9,12 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 
-def kOptimos(data):
+def kOptimosAnnosKilometraje(data):
     """
     Funcion que calculara la el numero optimo de clusters
-    para realizar los agrupamientos de los datos
+    para realizar los agrupamientos de los datos utilizando
+    el kilometraje y anno de fabricacion de los autos como
+    caracteristicas
     """
 
     # Caracteristicas a partir de las cuales se realizaran
@@ -36,7 +38,7 @@ def kOptimos(data):
     # codo para buscar cual k es una buena eleccion
     # Se define una grafico de tipo lineal
     plt.plot(range(1, 21), inertias, marker='o', color='red')
-    plt.title('Metodo del codo')
+    plt.title('Metodo del codo kilometraje y anno de fabricacion')
     plt.xlabel('Numero de Clusters (k)')
     plt.ylabel('Inercia')
 
@@ -44,10 +46,12 @@ def kOptimos(data):
     plt.show()
 
 
-def agrupamientoDeDatos(data):
+def agrupamientoDeDatosAnnosKilometraje(data):
     """
     Con este funcion se agrupan datos
-    utilizando el metodo Kmeans
+    utilizando el metodo Kmeans, utilizando como
+    caracteristicas el kilometraje y el anno de
+    fabricacion de los autos
     """
 
     # Caracteristicas a partir de las cuales se realizaran
@@ -71,4 +75,77 @@ def agrupamientoDeDatos(data):
     plt.scatter(data['year'], data['km_driven'], c=data['cluster_kmeans'],
                 cmap='viridis', marker='.')
     plt.title('Agrupamiento de los vehiculos segun sus caracteristicas')
+    plt.xlabel('Anno de fabricacion')
+    plt.ylabel('Kilometraje')
+    plt.show()
+
+
+def kOptimosAnnosPrecio(data):
+    """
+    Funcion que calculara la el numero optimo de clusters
+    para realizar los agrupamientos de los datos utilizando
+    el precio y anno de fabricacion de los autos como
+    caracteristicas
+    """
+
+    # Caracteristicas a partir de las cuales se realizaran
+    # los agrupamientos en K-Means
+    X_cluster = data[['year', 'selling_price']]
+
+    # Se Calcula la inercia para diferentes valores de k
+    # A partir de eso se grafica la varianza en funcion de k,
+    # y se busca el codo de la curva, el punto donde la inercia
+    # deja de disminuir rapidamente, ese valor de k es una buena eleccion
+    inertias = []
+    for k in range(1, 21):
+        # Se utiliza kmeans con diferentes valores de k
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        # Se ajusta el modelo
+        kmeans.fit(X_cluster)
+        # Se obtiene la inercia del kmeans obtenido
+        inertias.append(kmeans.inertia_)
+
+    # Se realiza un grafico utilizando el metodo del
+    # codo para buscar cual k es una buena eleccion
+    # Se define una grafico de tipo lineal
+    plt.plot(range(1, 21), inertias, marker='o', color='red')
+    plt.title('Metodo del codo precio y anno de fabricacion')
+    plt.xlabel('Numero de Clusters (k)')
+    plt.ylabel('Inercia')
+
+    # Se imprime la grafica en pantalla
+    plt.show()
+
+
+def agrupamientoDeDatosAnnosPrecio(data):
+    """
+    Con este funcion se agrupan datos
+    utilizando el metodo Kmeans, utilizando como
+    caracteristicas el precio y el anno de
+    fabricacion de los autos
+    """
+
+    # Caracteristicas a partir de las cuales se realizaran
+    # los agrupamientos en K-Means
+    X_cluster = data[['year', 'selling_price']]
+
+    # Se realiza el agrupamiento con KMeans, se crean 4 grupos
+    # n_clusters define el numero de grupos, random_state establece
+    # la forma en que se calculan los numeros aleatorios, n_init
+    # define las veces que el algoritmo de KMeans va a ser inicializado
+    # cada vez con centroides diferentes, esto realiza pruebas y la
+    # mejor de las 10 pruebas sera el resultado final
+    kmeans = KMeans(n_clusters=4, random_state=42, n_init=10)
+
+    # Se ajuste el modelo con los datos entregados
+    data['cluster_kmeans'] = kmeans.fit_predict(X_cluster)
+
+    # Se visualiza el agrupamiento por medio de un
+    # grafico de dispersion, el argumento c define
+    # que colores se utilizaran para el grafico
+    plt.scatter(data['year'], data['selling_price'], c=data['cluster_kmeans'],
+                cmap='viridis', marker='.')
+    plt.title('Agrupamiento de los vehiculos segun sus caracteristicas')
+    plt.xlabel('Anno de fabricacion')
+    plt.ylabel('Precio')
     plt.show()
